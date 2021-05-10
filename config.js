5a70c57bdf4ae99b8a338beee4364b8f8138d678
voidecker <62289663+voidecker@users.noreply.github.com>
2021-05-10 09:00:22 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.addEventListener('viewerLoaded', () => {
+  readerControl.disableFeatures([readerControl.Feature.LocalStorage]);
   readerControl.loadDocument('https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf');
 });
