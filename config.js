e252c7a2aa2f333428a563f4430c831d4817d913
voidecker <62289663+voidecker@users.noreply.github.com>
2021-05-20 12:37:58 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,26 +1,4 @@
 window.addEventListener('viewerLoaded', () => {
   readerControl.disableFeatures([readerControl.Feature.LocalStorage]);
   readerControl.loadDocument('https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf');
-
-  readerControl.docViewer.setWatermark({
-      // Draw diagonal watermark in middle of the document
-      diagonal: {
-        fontSize: 25, // or even smaller size
-        fontFamily: 'sans-serif',
-        color: 'red',
-        opacity: 50, // from 0 to 100
-        text: 'Watermark'
-      },
-
-      // Draw header watermark
-      header: {
-        fontSize: 10,
-        fontFamily: 'sans-serif',
-        color: 'red',
-        opacity: 70,
-        left: 'left watermark',
-        center: 'center watermark',
-        right: ''
-      }
-    });
 });
