7ec4c6d0dd38b7afd8d2b700b14ca4cf217e88e1
voidecker <62289663+voidecker@users.noreply.github.com>
2021-06-11 10:55:23 -0700

    Update drag-drop.js

--- drag-drop.js
+++ drag-drop.js
@@ -1,10 +1,22 @@
 let viewerWindow;
-const viewerElement = window.parent.document.getElementById('viewer');
-console.log(viewerElement);
-console.log(window);
-console.log(window.parent);
-console.log(window.parent.document);
-console.log(window.document);
+
 setTimeout(() => {
-  console.log(window.document.getElementById('viewer'));
+  const viewerElement = window.parent.document.getElementById('viewer');
+  console.log(viewerElement);
+  
+  viewerElement.addEventListener('ready', () => {
+    viewerWindow = viewerElement.querySelector('iframe').contentWindow;
+    viewerWindow.document.body.addEventListener('dragover', e => {
+        e.preventDefault();
+        return false;
+    });
+    viewerWindow.document.body.addEventListener('drop', e => {
+        const scrollElement = viewerWindow.docViewer.getScrollViewElement();
+        const scrollLeft = scrollElement.scrollLeft || 0;
+        const scrollTop = scrollElement.scrollTop || 0;
+        viewerWindow.setDropPoint({ x: e.pageX + scrollLeft, y: e.pageY + scrollTop });
+        e.preventDefault();
+        return false;
+    });
+  });
 }, 5000);
