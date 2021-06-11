02c87a4d0b4480393d4acd1095e3d44a7e70774f
voidecker <62289663+voidecker@users.noreply.github.com>
2021-06-11 10:51:21 -0700

    Update drag-drop.js

--- drag-drop.js
+++ drag-drop.js
@@ -2,5 +2,6 @@ let viewerWindow;
 const viewerElement = window.parent.document.getElementById('viewer');
 console.log(viewerElement);
 console.log(window);
+console.log(window.parent);
 console.log(window.document);
 console.log(window.document.getElementById('viewer'));
