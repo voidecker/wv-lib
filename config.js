cebb2d881651d5cea3223b3fad0f6eb8f4d79532
voidecker <62289663+voidecker@users.noreply.github.com>
2021-08-04 15:27:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.addEventListener('viewerLoaded', () => {
-  // readerControl.disableFeatures([readerControl.Feature.LocalStorage]);
-  readerControl.loadDocument('https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf');
+  // instance.disableFeatures([readerControl.Feature.LocalStorage]);
+  instance.loadDocument('https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf');
 });
