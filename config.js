7d249f1b40d9077aec9d351a76feeddfc4962645
voidecker <62289663+voidecker@users.noreply.github.com>
2022-05-03 11:45:52 -0700

    Remove alerts and add content edit btn

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-window.alert('hello outside viewerLoaded');
 window.addEventListener('viewerLoaded', () => {
   // instance.disableFeatures([readerControl.Feature.LocalStorage]);
-  window.alert('hello inside viewerLoaded');
   instance.loadDocument('https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf');
 });
+
+window.addEventListener('documentLoaded', () => {
+  instance.UI.enableElements(['contentEditButton']);
+})
