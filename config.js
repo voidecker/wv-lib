23a7d19d56ff559b0d87ecdeb2cb8a90552d2a2c
voidecker <62289663+voidecker@users.noreply.github.com>
2022-05-03 11:33:02 -0700

    adding test alerts

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+window.alert('hello outside viewerLoaded');
 window.addEventListener('viewerLoaded', () => {
   // instance.disableFeatures([readerControl.Feature.LocalStorage]);
+  window.alert('hello inside viewerLoaded');
   instance.loadDocument('https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf');
 });
